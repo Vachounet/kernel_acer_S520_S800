d9e0cdf0b28586a8bce4f457d67c689f50cacf3e
Shreps <shreps@otaku-irc.fr>
2014-01-09 11:31:09 +0100

    + Hidden config files

---
+++ .missing-syscalls.d
@@ -0,0 +1,7 @@
+-:  /home/ravena/OpenSource/a12ltev/kernel/include/linux/kconfig.h \
+ include/generated/autoconf.h \
+ /home/ravena/OpenSource/a12ltev/kernel/arch/arm/include/asm/types.h \
+ include/asm-generic/int-ll64.h \
+ arch/arm/include/generated/asm/bitsperlong.h \
+ include/asm-generic/bitsperlong.h \
+ /home/ravena/OpenSource/a12ltev/kernel/arch/arm/include/asm/unistd.h
