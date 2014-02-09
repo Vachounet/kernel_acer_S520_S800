6942439d4ba01541c3356b850384808c6b4d28c0
Vachounet <vachounet@live.fr>
2014-02-09 13:33:04 +0100

    kgsl: Implement conservative scaling policy
    This behaves similarly to the conservative cpufreq governor,
    which is similar to how most Mali GPU scaling algorithms behave.
    
    The "trustzone" policy that is default relies on an undocumented
    black box to make scaling decisions.
    
    How to enable:
    echo conservative > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/pwrscale/policy
    
    Conservative attr group:
    /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/pwrscale/policy_config/conservative
    
    Polling interval (10-1000):
    /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/pwrscale/policy_config/conservative/polling_interval
    
    Enable stats printing (causes microlags on low polling intervals):
    echo 1 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/pwrscale/policy_config/conservative/print_stats
    
    NOTE:
    - perms are set to 666 for testing purposes
    
    Change-Id: I9d854a73981515fc9850104302804928b04c4687
    Signed-off-by: Andrew Dodd <atd7@cornell.edu>
    
    Conflicts:
    
    	drivers/gpu/msm/kgsl_pwrscale.c

--- drivers/gpu/msm/kgsl_pwrscale.c
+++ drivers/gpu/msm/kgsl_pwrscale.c
@@ -48,9 +48,7 @@ static struct kgsl_pwrscale_policy *kgsl_pwrscale_policies[] = {
 #ifdef CONFIG_MSM_SLEEP_STATS_DEVICE
 	&kgsl_pwrscale_policy_idlestats,
 #endif
-#ifdef CONFIG_MSM_DCVS
-	&kgsl_pwrscale_policy_msm,
-#endif
+	&kgsl_pwrscale_policy_conservative,
 	NULL
 };
 
