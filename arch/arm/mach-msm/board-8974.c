c7d00c6568f0e5f0ac48f6abe02a461d3af20703
Vachounet <vachounet@live.fr>
2014-02-11 22:19:39 +0100

    Revert "intelli_thermal"
    Revert "intelli/msm_thermal: initial coding for Qualcomm Snapdragon 800 devices"
    
    This reverts commit 2fab59459c84edfd8fcff63bd10156a576fe5fb6.
    Revert "defconfig: intelli_thermal + fixes"
    
    This reverts commit fcd5df75549a70b475dd4f61788046c6949a0df1.
    
    Conflicts:
    	arch/arm/configs/d800_defconfig
    	arch/arm/configs/d801_defconfig
    	arch/arm/configs/d802_defconfig
    	arch/arm/configs/d803_defconfig
    	arch/arm/configs/ls980_defconfig
    	arch/arm/configs/vs980_defconfig

--- arch/arm/mach-msm/board-8974.c
+++ arch/arm/mach-msm/board-8974.c
@@ -47,6 +47,7 @@
 #include "modem_notifier.h"
 #include "lpm_resources.h"
 #include "platsmp.h"
+#include <mach/board_lge.h>
 
 
 static struct memtype_reserve msm8974_reserve_table[] __initdata = {
@@ -103,11 +104,7 @@ void __init msm8974_add_drivers(void)
 	else
 		msm_clock_init(&msm8974_clock_init_data);
 	tsens_tm_init_driver();
-#ifdef CONFIG_INTELLI_THERMAL
-	msm_thermal_init(NULL);
-#else
 	msm_thermal_device_init();
-#endif
 }
 
 static struct of_dev_auxdata msm8974_auxdata_lookup[] __initdata = {
